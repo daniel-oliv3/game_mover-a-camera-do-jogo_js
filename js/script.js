aea57820cfa63dacf45f22fd4e166135e4068155
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-06 10:47:36 -0300

    Background

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
     var cnv = document.querySelector('canvas');
     var ctx = cnv.getContext('2d');
 
+    //Recursos do jogo 
+    var background =  new Image();
+    background.src = "img/scene.png";
 
     function loop(){
         window.requestAnimationFrame(loop, cnv);
@@ -14,7 +17,8 @@
     }
 
     function render(){
-        
+        ctx.drawImage(background, 0, 0, 800, 600, 0, 0, 800, 600);
     }
 
-});
+    loop();
+}());
